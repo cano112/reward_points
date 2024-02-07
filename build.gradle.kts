3bed41addd0b12ed063824f93f1cf6cc0cf198a5
Kamil Noster <kamil.noster@gmail.com>
2024-02-07 17:35:21 +0100

    Integration test (happy path)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+ext["jooq.version"] = "3.19.3"
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.jooq.jooq-codegen-gradle") version "3.19.3"
 }
 
 group = "com.noster"
@@ -18,13 +21,64 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core:10.7.1")
+
+	runtimeOnly("org.flywaydb:flyway-database-postgresql:10.7.1")
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
+	testImplementation("io.rest-assured:spring-mock-mvc:5.4.0")
+	testImplementation("uk.co.datumedge:hamcrest-json:0.2")
+	jooqCodegen("org.postgresql:postgresql")
+	jooqCodegen("org.jooq:jooq-meta-extensions:3.19.3")
+}
+
+jooq {
+	configuration {
+		logging = org.jooq.meta.jaxb.Logging.INFO
+		generator {
+			database {
+				name = "org.jooq.meta.extensions.ddl.DDLDatabase"
+				properties {
+					property {
+						key = "scripts"
+						value = "src/main/resources/db/migration/*.sql"
+					}
+
+					property {
+						key = "sort"
+						value = "flyway"
+					}
+
+					property {
+						key = "defaultNameCase"
+						value = "lower"
+					}
+				}
+			}
+		}
+	}
+}
+
+tasks.named("compileJava") {
+	dependsOn(":jooqCodegen")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+	options.compilerArgs.add("--enable-preview")
+}
+
+tasks.withType<Test>().configureEach {
+	jvmArgs("--enable-preview")
+}
+
+tasks.withType<JavaExec>().configureEach {
+	jvmArgs("--enable-preview")
 }
--- src/test/java/com/noster/rewardpoints/RewardPointsApplicationTests.java
+++ src/test/java/com/noster/rewardpoints/RewardPointsApplicationTests.java
@@ -1,22 +1,120 @@
 package com.noster.rewardpoints;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.jooq.DSLContext;
+import org.jooq.generated.Tables;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @Testcontainers
 class RewardPointsApplicationTests {
 
-	@Container
-	@ServiceConnection
-	private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16");
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16");
 
-	@Test
-	void contextLoads() {
-	}
+    private final DSLContext jooq;
+
+    private final MockMvc mockMvc;
+
+    @Autowired
+    RewardPointsApplicationTests(DSLContext jooq, MockMvc mockMvc) {
+        this.jooq = jooq;
+        this.mockMvc = mockMvc;
+    }
+
+    @AfterEach
+    void clearDb() {
+        jooq.truncate(Tables.REWARD_POINTS).execute();
+    }
+
+    @Test
+    void shouldReturnMonthlyAndTotalRewardPointsSummary_givenTransactionRecordPresent() {
+        final var userId = UUID.fromString("fa4deba2-3954-45a0-82c9-582244e1d135");
+        givenTransaction(BigDecimal.valueOf(120), Instant.parse("2020-09-01T10:00:00Z"), userId);
+        givenTransaction(BigDecimal.valueOf(120), Instant.parse("2020-10-01T10:00:00Z"), userId);
+        givenTransaction(BigDecimal.valueOf(100), Instant.parse("2020-10-02T10:00:00Z"), userId);
+        givenTransaction(BigDecimal.valueOf(100), Instant.parse("2020-11-01T10:00:00Z"), userId);
+        givenTransaction(BigDecimal.valueOf(100), Instant.parse("2020-12-01T10:00:00Z"), userId);
+
+        given()
+                .mockMvc(mockMvc)
+                .when()
+                .get(STR."/users/\{userId}/rewards/points/summary", Map.of(
+                        "start", "2020-10-01T00:00:00Z",
+                        "end", "2020-12-31:23:59:59Z",
+                        "aggregations", "MONTHLY,TOTAL"
+                ))
+                .then()
+                .status(HttpStatus.OK)
+                .body(sameJSONAs(
+                        //language=JSON
+                        """
+                                {
+                                    "user_id": "fa4deba2-3954-45a0-82c9-582244e1d135",
+                                    "period": {
+                                        "start": "2020-10-01T00:00:00Z",
+                                        "end": "2020-12-31:23:59:59Z"
+                                    },
+                                    "aggregations": {
+                                        "monthly": [
+                                            {
+                                                "month": 10,
+                                                "points": 140
+                                            },
+                                            {
+                                                "month": 11,
+                                                "points": 50
+                                            },
+                                            {
+                                                "month": 12,
+                                                "points": 50
+                                            }
+                                        ],
+                                        "total": 240
+                                    }
+                                }
+                                    """
+                ));
+
+    }
+
+    private void givenTransaction(BigDecimal amount, Instant timestamp, UUID userId) {
+        given()
+                .mockMvc(mockMvc)
+                .when()
+                .post(STR."/users/\{userId}/rewards/transactions", STR."""
+                        {
+                        	"transaction_id": "\{UUID.randomUUID()}",
+                        	"transaction_amount": \{amount},
+                            "currency": "USD",
+                            "timestamp": "\{DateTimeFormatter.ISO_INSTANT.format(timestamp)}"
+                        }
+                        """)
+                .then()
+                .status(HttpStatus.CREATED);
+    }
 
 }
